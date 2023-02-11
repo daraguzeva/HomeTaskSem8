dc555820b0b473c314469045c18dc3b5fc94d75b
Daria <lara-lorn@yandex.ru>
2023-02-11 15:28:35 +0400

    save

--- Task058/Program.cs
+++ Task058/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+int[,] arr1 = new int[2, 2];
+for (int i = 0; i < arr1.GetLength(0); i++)
+{
+    for (int j = 0; j < arr1.GetLength(1); j++)
+    {
+        arr1[i, j] = new Random().Next(0, 10);
+        Console.Write($"{arr1[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+int[,] arr2 = new int[2, 2];
+for (int i = 0; i < arr2.GetLength(0); i++)
+{
+    for (int j = 0; j < arr2.GetLength(1); j++)
+    {
+        arr2[i, j] = new Random().Next(0, 10);
+        Console.Write($"{arr2[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+int[,] array = new int[2, 2];
+array[0, 0] = arr1[0, 0] * arr2[0, 0] + arr1[1, 0] * arr2[1, 0];
+
